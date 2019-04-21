0dd669513901d0023d57c6b05990e1c1d456b871
alhanoufoth33 <alhanoufoth33@gmail.com>
2019-04-21 16:21:12 +0300

    updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+List.create(tiltle: 'lemon', description: '4 killos')
