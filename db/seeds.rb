1a7c14e89253a10ebd59cb228f83aaf181efc6ab
khuzamas <khuzamshu@gmail.com>
2019-04-23 16:58:50 +0300

    navbar edited

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Item.destroy_all
 
 List.create(tiltle: 'lemon', description: '4 killos')
 
-User.destroy_all
+# User.destroy_all
 
 User.create(email: "ri1n1g")
 User.create(email: "ri1nfffff")
@@ -23,7 +23,7 @@ User.create(email: "eeeeeee")
 
 Option.destroy_all
 
-Option.create(name: "1", category: "food", image: "https://image.flaticon.com/icons/png/512/45/45332.png")
+Option.create(name: "watermelon", category: "fruit", image: "2.png")
 Option.create(name: "2", category: "food", image: "https://image.flaticon.com/icons/png/512/45/45332.png")
 Option.create(name: "3", category: "food", image: "https://image.flaticon.com/icons/png/512/45/45332.png")
 Option.create(name: "4", category: "food", image: "https://image.flaticon.com/icons/png/512/45/45332.png")
