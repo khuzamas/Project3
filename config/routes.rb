b94565e9810f669207fb507dd1e686313787789d
khuzamas <khuzamshu@gmail.com>
2019-04-24 19:55:36 +0300

    home page edited

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
 
 
-  root "fridges#index"
+  
   devise_for :users
   resources :lists
 
@@ -11,7 +11,20 @@ Rails.application.routes.draw do
     resources :items
   end
 
+  get 'fridges/:id/empty' => 'fridges#empty'
   get 'fridges/:id/add_user' => 'fridges#add_user'
   patch 'fridges/:id/update_user' => 'fridges#update_user'
+
+
+  devise_scope :user do
+    authenticated :user do
+      root 'fridges#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'fridges#home', as: :unauthenticated_root
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
