424befead2d6a61beced94cf1a938fa1ffe62eb4
khuzamas <khuzamshu@gmail.com>
2019-04-24 16:53:44 +0300

    changed

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_24_103430) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "fridges", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
